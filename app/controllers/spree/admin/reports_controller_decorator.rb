9703155d1ecff50e92be396fc9e242013b16a1e2
Rogelio Meza <rogelio.meza.t@gmail.com>
2017-07-28 01:17:26 -0400

    use search params

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -6,8 +6,21 @@ module Spree
       def out_of_stock
         params[:q] = {} unless params[:q]
 
-        @search = Variant.ransack(params[:q])
-        @variants = @search.result
+        #by default, the first date is the beginning of month
+        if params[:q][:created_at_gt].blank?
+          params[:q][:created_at_gt] = Time.zone.now.beginning_of_month
+        else
+          params[:q][:created_at_gt] = Time.zone.parse(params[:q][:created_at_gt]).beginning_of_day rescue Time.zone.now.beginning_of_month
+        end
+
+        if params[:q] && !params[:q][:created_at_lt].blank?
+          params[:q][:created_at_lt] = Time.zone.parse(params[:q][:created_at_lt]).end_of_day rescue ""
+        end
+
+        params[:q][:s] ||= "created_at desc"
+
+        @search = OutOfStock.ransack(params[:q])
+        @out_of_stocks = @search.result
       end
     end
   end
